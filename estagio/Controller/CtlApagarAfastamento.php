86e826ee6bf7510216cb8f99c7144eb3534f9eef
Home <Home@192.168.0.100>
2013-09-30 00:33:57 -0300

    Criação ctlApagarAfastamento

---
+++ estagio/Controller/CtlApagarAfastamento.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once '../DataAccess/AfastamentoDAO.php';
+include_once '../DomainModel/Afastamento.php';
+
+
+if (!isset($_SESSION))
+    session_start();
+$nivel_necessario = 1;
+// Verifica se não há a variavel da sessao que identifica o usuario
+if (!isset($_SESSION['usuarioNome']) OR ($_SESSION['usuarioNivel'] < $nivel_necessario)) {
+    // Destr?i a sess?o por seguran?a
+    session_destroy();
+    // Redireciona o visitante de volta pro login
+    header("Location: index_.php");
+    exit; // mudar depois dos testes
+}
+
+$id = $_GET['id'];
+
+
+$dao = new AfastamentoDAO();
+$afastamento = new Afastamento();
+
+$afastamento->setIdAfastamento($id);
+
+$dao->apagar($afastamento);
+echo '<script type="text/javascript"> alert("Apagado com Sucesso !"); window.location="../Presentation/main.php?pagina=frmCadastroP.php";</script>';
+?>
+
