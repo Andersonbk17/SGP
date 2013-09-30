7999720bf3a49cbff11e6c7c45a0a8e4b21b3e77
Home <Home@192.168.0.100>
2013-09-29 23:48:39 -0300

    Criação CtlEditarDependente

---
+++ estagio/Controller/CtlEditarDependente.php
@@ -0,0 +1,41 @@
+<?php
+    include_once '../DataAccess/DependenteDAO.php';
+    include_once '../DomainModel/Dependente.php';
+    
+    
+    if (!isset($_SESSION)) session_start();
+	 $nivel_necessario = 1;
+	// Verifica se não há a variavel da sessao que identifica o usuario
+	if (!isset($_SESSION['usuarioNome']) OR ($_SESSION['usuarioNivel'] < $nivel_necessario)) {
+ 	// Destr?i a sess?o por seguran?a
+	    session_destroy();
+	// Redireciona o visitante de volta pro login
+	    header("Location: index_.php"); exit; // mudar depois dos testes
+	}
+ 
+    
+    
+    $id = $_POST['id'];
+    $nome = $_POST['nome'];
+    $dataNascimento = implode("-",array_reverse(explode("/",$_POST['dataNascimento'])));
+    $sexo = $_POST['sexo'];   
+    $idFuncionario = $_POST['funcionario'];   
+ 
+    
+    
+    
+    
+    $dao = new DependenteDAO();
+    $dependente = new Dependente();
+    
+    $dependente->setId($id);
+    $dependente->setDataNascimento($dataNascimento);
+    $dependente->setNome($nome);
+    $dependente->setSexo($sexo);
+    $dependente->setIdFuncionario($idFuncionario);
+    
+    $dao->alterar($dependente);
+     echo '<script type="text/javascript"> alert("Alterado com Sucesso !"); window.location="../Presentation/main.php?pagina=frmCadastroP.php";</script>';
+        
+
+?>
