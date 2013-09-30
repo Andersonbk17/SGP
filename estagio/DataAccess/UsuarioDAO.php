d49e4b8bee28bb9bdc8bf4a88e8560eda95ec355
Home <Home@192.168.0.100>
2013-09-30 08:18:45 -0300

    Criação funções Apagar, Atualizar e edição na função abrir na classe UsuarioDAO

--- estagio/DataAccess/UsuarioDAO.php
+++ estagio/DataAccess/UsuarioDAO.php
@@ -1,59 +1,66 @@
 <?php
- include_once 'Conexao.php';
- include_once '../DomainModel/Usuario.php';
-class UsuarioDAO {
-    
-    function __construct(){
-		
-		}
-   public function abrir( $id){
-                $sql = "SELECT  * FROM usuario WHERE idUsuario = $id ";
+
+    include_once 'Conexao.php';
+    include_once '../DomainModel/Usuario.php';
+
+    class UsuarioDAO {
+
+        function __construct() {
+
+        }
+
+        public function abrir($id) {
+            $sql = "SELECT  * FROM usuario WHERE status = 1 AND idUsuario =  $id ";
+            $novo = new Usuario();
+
+            $rs = mysql_query($sql);
+            while ($resultado = mysql_fetch_array($rs)) {
+                $novo->setId(stripslashes($resultado['idUsuario']));
+                $novo->setUsuario(stripslashes($resultado['usuario']));
+                $novo->setSenha(stripcslashes($resultado['senha']));
+                $novo->setNivel(stripcslashes($resultado['nivel']));
+                return $novo;
+            }
+        }
+
+        public function inserir($obj) {
+            $sql = sprintf("INSERT INTO usuario(usuario,senha,nivel,idFuncionario,status) VALUES('%s','%s','%d','%d',1)", $obj->getUsuario(), $obj->getSenha(), $obj->getNivel(), $obj->getIdFuncionario());
+            mysql_query($sql)
+                    OR die(mysql_error());
+        }
+
+        public function atualizar(Usuario $obj) {
+            $sql = sprintf("UPDATE usuario SET usuario='%s',senha='%s',nivel='%s',idFuncionario='%s' WHERE idUsuario = '%s' ", $obj->getUsuario(), $obj->getSenha(), $obj->getNivel(), $obj->getIdFuncionario(), $obj->getId());
+            mysql_query($sql)
+                    OR die(mysql_error());
+        }
+
+        public function apagar(Usuario $obj) {
+            $sql = sprintf("UPDATE usuario SET status = 0 WHERE idUsuario = '%s' ", $obj->getId());
+            mysql_query($sql)
+                    OR die(mysql_error());
+        }
+
+        public function ListarTodos() {
+
+            $sql = "SELECT  * FROM usuario WHERE status = 1";
+            $lista = new ArrayObject();
+
+            $resultado = mysql_query($sql);
+
+            while ($rs = mysql_fetch_array($resultado)) {
+
                 $novo = new Usuario();
 
-                $resultado = mysql_query($sql);
-                if($resultado) {
-                    $novo->setId(stripslashes($resultado['idUsuario']));
-                    $novo->setUsuario(stripslashes($resultado['usuario']));
-                    $novo->setSenha(stripcslashes($resultado['senha']));
-                    $novo->setNivel(stripcslashes($resultado['nivel']));
-                    return $novo;
-                }else{
-                    return null;
-                }
-                
-	}
-        
-    
-          public function inserir($obj){
-			$sql = sprintf("INSERT INTO usuario(usuario,senha,nivel,idFuncionario,status) VALUES('%s','%s','%d','%d',1)",$obj->getUsuario(),
-                                $obj->getSenha(),$obj->getNivel(),$obj->getIdFuncionario());
-			mysql_query($sql)
-                        OR die(mysql_error());
-                        
+                $novo->setId(stripslashes($rs['idUsuario']));
+                $novo->setUsuario(stripslashes($rs['usuario']));
+                $novo->setSenha(stripslashes($rs['senha']));
+                $novo->setNivel(stripslashes($rs['nivel']));
+                $lista->append($novo);
+            }
+            return $lista;
         }
-        
-        public function ListarTodos(){
-		
-			$sql = "SELECT  * FROM usuario WHERE status = 1";
-			$lista = new ArrayObject();
-			
-			$resultado = mysql_query($sql);
-                        
-			while($rs = mysql_fetch_array($resultado)){
-			
-				$novo = new Usuario();
-				
-				$novo->setId(stripslashes($rs['idUsuario']));
-				$novo->setUsuario(stripslashes($rs['usuario']));
-                                $novo->setSenha(stripslashes($rs['senha']));
-                                $novo->setNivel(stripslashes($rs['nivel']));
-				$lista->append($novo);
-			}
-			return $lista;
-		
-		}
-        
-    
-}
+
+    }
 
 ?>
